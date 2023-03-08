d9feb89805a0a93ca0cec3d82283d3507f6b39f7
Dhananjai Sharma <dhananjaisharma09@gmail.com>
2023-03-09 01:23:20 +0530

    drawer added

--- fuwa/src/lib/components/(chat)/UserDrawer/UserDrawer.svelte
+++ fuwa/src/lib/components/(chat)/UserDrawer/UserDrawer.svelte
@@ -1,32 +1,55 @@
 <script>
 	import Clock from './Clock.svelte';
 	import Avatar from 'svelte-avatar';
-
+	import LeftArrow from '$lib/assets/svg/leftArrow.svelte';
 	export let user;
 </script>
 
-<div class="drawer">
-	<div class="top">
-		<Clock />
+<div class="drawer drawer-end">
+	<input id="my-drawer-4" type="checkbox" class="drawer-toggle" />
+	<div class="drawer-content">
+		<div class="user-drawer">
+			<div class="top">
+				<Clock />
+			</div>
+			<div class="bottom">
+				<div class="tag">
+					<Avatar name={user.name} src={user.profile_picture} size="80px" loading="false" />
+					<span class="user_name">{user.name}</span>
+				</div>
+				<div class="menu">
+					<span>Change Profile Picture</span>
+					<span>Change Username</span>
+					<label for="my-drawer-4">Settings</label>
+				</div>
+				<div class="log-out">
+					<button class="log-out-btn">Log Out</button>
+				</div>
+			</div>
+		</div>
 	</div>
-	<div class="bottom">
-		<div class="tag">
-			<Avatar name={user.name} src={user.profile_picture} size="80px" />
-			<span class="user_name">{user.name}</span>
-		</div>
-		<div class="menu">
-			<span>Change Profile Picture</span>
-			<span>Change Username</span>
-			<span>Settings</span>
-		</div>
-		<div class="log-out">
-			<button class="log-out-btn">Log Out</button>
+
+	<div class="drawer-side">
+		<label for="my-drawer-4" class="drawer-overlay" />
+		<div class="settings">
+			<div class="menu-top">
+				<label for="my-drawer-4" class="menu-back"><LeftArrow /></label>
+				<span class="menu-head">Settings</span>
+			</div>
+			<div class="menu-drawer">
+				<span>Theme</span>
+				<span>Live Wallpaper</span>
+				<span>Privacy</span>
+				<span>Security</span>
+				<span>Notifications</span>
+				<span>About</span>
+			</div>
 		</div>
 	</div>
 </div>
 
 <style lang="scss">
-	.drawer {
+	.user-drawer {
 		height: 100%;
 		display: flex;
 		flex-direction: column;
@@ -72,5 +95,34 @@
 		color: white;
 		font-size: 1em;
 		margin-top: 1em;
+	}
+	.settings {
+		background-color: #161616;
+		width: 100%;
+		height: 100%;
+		padding: 0.75em;
+	}
+	.menu-top {
+		display: flex;
+		justify-content: center;
+		width: 100%;
+	}
+	.menu-head {
+		width: 90%;
+		text-align: center;
+		font-size: 20px;
+		line-height: 23px;
+		color: white;
+	}
+	.menu-back {
+		width: 10%;
+	}
+	.menu-drawer {
+		display: flex;
+		flex-direction: column;
+		color: white;
+		font-family: sans-serif;
+		line-height: 3em;
+		font-size: 1.25em;
 	}
 </style>
