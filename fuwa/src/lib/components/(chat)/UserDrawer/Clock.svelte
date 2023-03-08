f5b08dd82ab520b3663708bb99a22da2445a772c
Dhananjai Sharma <dhananjaisharma09@gmail.com>
2023-03-09 00:12:29 +0530

    user drawer added

--- fuwa/src/lib/components/(chat)/UserDrawer/Clock.svelte
+++ fuwa/src/lib/components/(chat)/UserDrawer/Clock.svelte
@@ -21,7 +21,7 @@
 </script>
 
 <div class="time-wrap">
-	<span class="time">{hours} : {minutes}</span>
+	<span class="time">{hours < 10 ? '0' : ''}{hours} : {minutes < 10 ? '0' : ''}{minutes}</span>
 	<span class="date">{date < 10 ? '0' : ''}{date} / {month + 1 < 10 ? '0' : ''}{month + 1}</span>
 	<span class="weekday">{weekday[day]}</span>
 </div>
--- fuwa/src/lib/components/(chat)/UserDrawer/UserDrawer.svelte
+++ fuwa/src/lib/components/(chat)/UserDrawer/UserDrawer.svelte
@@ -1,5 +1,6 @@
 <script>
 	import Clock from './Clock.svelte';
+	import Avatar from 'svelte-avatar';
 
 	export let user;
 </script>
@@ -9,22 +10,67 @@
 		<Clock />
 	</div>
 	<div class="bottom">
-		<h1>{user.name}</h1>
+		<div class="tag">
+			<Avatar name={user.name} src={user.profile_picture} size="80px" />
+			<span class="user_name">{user.name}</span>
+		</div>
+		<div class="menu">
+			<span>Change Profile Picture</span>
+			<span>Change Username</span>
+			<span>Settings</span>
+		</div>
+		<div class="log-out">
+			<button class="log-out-btn">Log Out</button>
+		</div>
 	</div>
 </div>
 
 <style lang="scss">
 	.drawer {
-		padding: 2em;
 		height: 100%;
 		display: flex;
 		flex-direction: column;
 	}
 	.top {
-		height: 80%;
+		padding: 2em;
+		height: 65%;
 	}
 	.bottom {
-		height: 20%;
-		background-color: beige;
+		height: 35%;
+		padding: 1em;
+		background-color: #161616;
+	}
+	.tag {
+		display: flex;
+		align-items: center;
+	}
+	.user_name {
+		font-size: 2.25em;
+		margin-left: 0.5em;
+		color: white;
+	}
+	.menu {
+		display: flex;
+		flex-direction: column;
+		color: white;
+		font-family: sans-serif;
+		margin-top: 1em;
+		line-height: 2em;
+	}
+	.log-out {
+		display: flex;
+		width: 100%;
+		align-items: center;
+		justify-content: center;
+	}
+	.log-out-btn {
+		background: #a020f0;
+		border-radius: 15px;
+		border: 0;
+		width: 125px;
+		height: 35px;
+		color: white;
+		font-size: 1em;
+		margin-top: 1em;
 	}
 </style>
