8e4060a0c39bc2781aed93a53757245ece2b9327
Dhananjai Sharma <dhananjaisharma09@gmail.com>
2023-04-01 15:12:59 +0530

    steps + otp added

--- fuwa/src/routes/(auth)/auth/sign-up/details/+page.svelte
+++ fuwa/src/routes/(auth)/auth/sign-up/details/+page.svelte
@@ -1,24 +1,20 @@
-<script>
-	let curr = 'email/phone';
-</script>
-
 <div class="w-screen h-screen flex items-center justify-center flex-col">
 	<ul class="steps steps-vertical lg:steps-horizontal mb-12">
 		<li class="step step-primary">Email / Phone Number</li>
-		<li class="step">OTP</li>
-		<li class="step">Details</li>
+		<li class="step step-primary">OTP</li>
+		<li class="step step-primary">Details</li>
 		<li class="step">All Set</li>
 	</ul>
 	<div class="card w-96 bg-neutral-focus shadow-xl h-72">
 		<div class="card-body h-full">
-			<h1 class="card-title mb-12">Enter phone no. / e-mail</h1>
+			<h1 class="card-title mb-12">Enter your Details</h1>
 			<input
 				type="text"
 				placeholder="Type here"
 				class="input input-bordered input-accent w-full max-w-xs mb-12"
 			/>
 			<div class="card-actions justify-end">
-				<a href="./otp">
+				<a href="./fin">
 					<button class="btn btn-primary">Next</button>
 				</a>
 			</div>
