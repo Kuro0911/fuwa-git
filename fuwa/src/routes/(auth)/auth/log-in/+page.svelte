64c0d1fdc5beeb739f9ce0597ab2c83ac2372eea
Ayush Kumar <k.ayushprogramming@gmail.com>
2023-04-03 23:49:31 +0530

    log in feature added

---
+++ fuwa/src/routes/(auth)/auth/log-in/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+	import { auth } from '$lib/firebase';
+	import { signInWithEmailAndPassword } from 'firebase/auth';
+
+	let email = '';
+	let password = '';
+
+	const logIn = async () => {
+		try {
+			const userLogIn = await signInWithEmailAndPassword(auth, email, password);
+			console.log('User Log-in Successful!');
+			console.log(userLogIn);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+</script>
+
+Log-In Screen to be Made
+<!-- Just add logIn function as onclick on the submit button -->
