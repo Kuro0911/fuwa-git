ea742810a27c571474a4802a812bb9664cb971de
Ayush Kumar <k.ayushprogramming@gmail.com>
2023-04-03 18:23:03 +0530

    sign in feature added

--- fuwa/src/lib/firebase.js
+++ fuwa/src/lib/firebase.js
@@ -1,19 +1,21 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore"
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDxC2ivWw1EYqrz-ZBOy97rFM66_6ynZnk",
-  authDomain: "fuwa-test.firebaseapp.com",
-  projectId: "fuwa-test",
-  storageBucket: "fuwa-test.appspot.com",
-  messagingSenderId: "1083488838868",
-  appId: "1:1083488838868:web:ae2cdab3f4e4b55ef80741"
+	apiKey: 'AIzaSyDxC2ivWw1EYqrz-ZBOy97rFM66_6ynZnk',
+	authDomain: 'fuwa-test.firebaseapp.com',
+	projectId: 'fuwa-test',
+	storageBucket: 'fuwa-test.appspot.com',
+	messagingSenderId: '1083488838868',
+	appId: '1:1083488838868:web:ae2cdab3f4e4b55ef80741'
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app)
+export const db = getFirestore(app);
+export const auth = getAuth(app);
--- fuwa/src/routes/(auth)/auth/sign-up/enter-email-phone/+page.svelte
+++ fuwa/src/routes/(auth)/auth/sign-up/enter-email-phone/+page.svelte
@@ -1,7 +1,8 @@
 <script>
 	let curr = 'email/phone';
-	import { db } from '../../../../../lib/firebase';
+	import { db, auth } from '../../../../../lib/firebase';
 	import { collection, addDoc, getDocs, setDoc, doc } from 'firebase/firestore';
+	import { createUserWithEmailAndPassword, onAuthStateChanged, updateProfile } from 'firebase/auth';
 	import { current_user } from '../../../../../lib/utils/store';
 
 	let user_id = '';
@@ -10,10 +11,41 @@
 		user_id = val;
 	});
 
-	let email_or_mobile = '';
+	let email = '';
+	let password = '';
+	let fullname = '';
 
-	const handleEmailOrMobile = (e) => {
-		email_or_mobile = e.target.value;
+	const handleEmail = (e) => {
+		email = e.target.value;
+	};
+	const handlePassword = (e) => {
+		password = e.target.value;
+	};
+	const handleFullName = (e) => {
+		fullname = e.target.value;
+	};
+
+	onAuthStateChanged(auth, async (user) => {
+		await updateProfile(auth.currentUser, {
+			displayName: fullname
+		});
+		if (user) {
+			console.log('u-email: ', user.email);
+			console.log('u-dn: ', user.displayName);
+		}
+	});
+
+	const signIn = async () => {
+		try {
+			const user = await createUserWithEmailAndPassword(auth, email, password);
+
+			addEmailToDetails();
+			email = '';
+			password = '';
+			fullname = '';
+		} catch (error) {
+			console.log(error);
+		}
 	};
 
 	const addEmailToDetails = async () => {
@@ -21,9 +53,9 @@
 			const data = {
 				active: true
 			};
-			const detailsRef = await setDoc(doc(db, `${email_or_mobile}`, 'details'), data);
-			current_user.set(email_or_mobile);
-			console.log(detailsRef);
+			const detailsRef = await setDoc(doc(db, `${email}`, 'details'), data);
+			current_user.set(email);
+			// console.log(detailsRef);
 		} catch (error) {
 			console.log(error);
 		}
@@ -37,18 +69,35 @@
 		<li class="step">Details</li>
 		<li class="step">All Set</li>
 	</ul>
-	<div class="card w-96 bg-neutral-focus shadow-xl h-72">
+	<div class="card w-96 bg-neutral-focus shadow-xl">
 		<div class="card-body h-full">
-			<h1 class="card-title mb-12">Enter phone no. / e-mail</h1>
+			<h1 class="card-title mb-6">Enter E-mail Address</h1>
+			<input
+				type="email"
+				placeholder="Type here"
+				class="input input-bordered input-accent w-full max-w-xs mb-12"
+				bind:value={email}
+				on:change={handleEmail}
+			/>
+			<h1 class="card-title mb-6">Enter Password</h1>
+			<input
+				type="password"
+				placeholder="Type here"
+				class="input input-bordered input-accent w-full max-w-xs mb-12"
+				bind:value={password}
+				on:change={handlePassword}
+			/>
+			<h1 class="card-title mb-6">Enter Full Name</h1>
 			<input
 				type="text"
 				placeholder="Type here"
 				class="input input-bordered input-accent w-full max-w-xs mb-12"
-				on:change={handleEmailOrMobile}
+				bind:value={fullname}
+				on:change={handleFullName}
 			/>
 			<div class="card-actions justify-end">
 				<a href="./otp">
-					<button class="btn btn-primary" on:click={addEmailToDetails}>Next</button>
+					<button class="btn btn-primary" on:click={signIn}>Next</button>
 				</a>
 			</div>
 		</div>
