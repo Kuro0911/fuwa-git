182d44f5b375943b3ff85c5fac0737722a64aa54
Dhananjai Sharma <dhananjaisharma09@gmail.com>
2023-04-19 18:17:42 +0530

    fixes

--- fuwa/src/routes/(chat)/chat/+page.svelte
+++ fuwa/src/routes/(chat)/chat/+page.svelte
@@ -1,1 +1,5 @@
-<h1>test</h1>
+<div
+	class="flex justify-center items-center h-full bg-[url('$lib/assets/svg/bg.svg')] bg-no-repeat bg-cover"
+>
+	<p class="text-2xl">Click on an Avatar or add a new friend to start chatting</p>
+</div>
