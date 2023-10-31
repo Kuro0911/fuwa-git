40640576da847eba2033dd8fff3448165d5a4a88
Kuro0911 <dhananjaisharma09@gmail.com>
2023-10-31 09:33:44 +0530

    changes

--- fuwa/src/routes/+page.svelte
+++ fuwa/src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <h1>Welcome to Fuwa</h1>
-<p>this path will probably be deleted so until we will just ignore it</p>
+<p>this path will probably be deleted so until we will just ignore it fr</p>
 
 <p>to go to chats <a href="/chat/test123">click here</a></p>
 <p>to go to auth <a href="/auth">click here</a></p>
